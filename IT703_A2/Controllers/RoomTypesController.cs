960a35db4f0a7ba57d4a150cfe481c2af6d7469b
yufunz <yufu@example.com>
2022-11-01 19:41:32 +1300

    implement room types all view

--- IT703_A2/Controllers/RoomTypesController.cs
+++ IT703_A2/Controllers/RoomTypesController.cs
@@ -7,155 +7,71 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using IT703_A2.Data;
 using IT703_A2.Models;
+using IT703_A2.Models.RoomTypes;
+using IT703_A2.Services;
 
 namespace IT703_A2.Controllers
 {
     public class RoomTypesController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IRoomTypesService roomTypeService;
 
-        public RoomTypesController(ApplicationDbContext context)
+        public RoomTypesController(IRoomTypesService rTService)
         {
-            _context = context;
+            this.roomTypeService = rTService;
         }
 
-        // GET: RoomTypes
-        public async Task<IActionResult> Index()
+        public IActionResult All([FromQuery] ListRoomTypeQueryModel rTQuery)
         {
-              return View(await _context.RoomTypes.ToListAsync());
+            var allRoomTypes = this.roomTypeService.ListTypes(rTQuery);
+
+            return this.View(allRoomTypes);
         }
 
-        // GET: RoomTypes/Details/5
-        public async Task<IActionResult> Details(string id)
+        public IActionResult Add()
         {
-            if (id == null || _context.RoomTypes == null)
-            {
-                return NotFound();
-            }
-
-            var roomType = await _context.RoomTypes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (roomType == null)
-            {
-                return NotFound();
-            }
-
-            return View(roomType);
+            return this.View();
         }
 
-        // GET: RoomTypes/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: RoomTypes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Rate,NumOfBeds,Image,IsDeleted")] RoomType roomType)
+        public async Task<IActionResult> Add(AddRoomTypeFormModel room)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                _context.Add(roomType);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return this.View(room);
             }
-            return View(roomType);
+
+            await this.roomTypeService.Add(room);
+
+            return this.RedirectToAction("All", "RoomTypes");
         }
 
-        // GET: RoomTypes/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null || _context.RoomTypes == null)
-            {
-                return NotFound();
-            }
-
-            var roomType = await _context.RoomTypes.FindAsync(id);
-            if (roomType == null)
-            {
-                return NotFound();
-            }
-            return View(roomType);
-        }
-
-        // POST: RoomTypes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Rate,NumOfBeds,Image,IsDeleted")] RoomType roomType)
-        {
-            if (id != roomType.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(roomType);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!RoomTypeExists(roomType.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(roomType);
-        }
-
-        // GET: RoomTypes/Delete/5
         public async Task<IActionResult> Delete(string id)
         {
-            if (id == null || _context.RoomTypes == null)
-            {
-                return NotFound();
-            }
+            await this.roomTypeService.Delete(id);
 
-            var roomType = await _context.RoomTypes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (roomType == null)
-            {
-                return NotFound();
-            }
-
-            return View(roomType);
+            return this.RedirectToAction("All", "RoomTypes");
         }
 
-        // POST: RoomTypes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
+        public IActionResult Edit(string id)
         {
-            if (_context.RoomTypes == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.RoomTypes'  is null.");
-            }
-            var roomType = await _context.RoomTypes.FindAsync(id);
-            if (roomType != null)
-            {
-                _context.RoomTypes.Remove(roomType);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            var currentType = this.roomTypeService.GetRoomType(id);
+
+            return this.View(currentType);
         }
 
-        private bool RoomTypeExists(string id)
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditRoomTypeFormModel rType)
         {
-          return _context.RoomTypes.Any(e => e.Id == id);
+            if (!ModelState.IsValid)
+            {
+                return this.View(rType);
+            }
+
+            await this.roomTypeService.Update(rType);
+
+            return this.RedirectToAction("All", "RoomTypes");
         }
+
     }
 }
